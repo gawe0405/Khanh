098dde2b2a472fdcaca11ccbb26a9771f1c93643
gawe0405@gmail.com <gawe0405@gmai.com>
2020-01-14 20:37:45 +0700

    Testgithub

---
+++ TestGithub.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> phep tinh voi 2 so </title>
+</head>
+<body>
+    <?php
+    $num=0;
+    for($i=1;$i<=100;$i++)
+    {
+        if($i%7==0)
+        {
+            $num=$num+1;
+            
+        }
+    }
+    echo "Tim duoc ".$num." so!" ;  
+    for($i=1;$i<=100;$i++)
+    {
+        if($i%7==0)
+        {
+            $num=$num+1;
+            echo "<br/>".$i;
+           
+        }
+    }     
+    
+    ?>
+</body>
+</html>
